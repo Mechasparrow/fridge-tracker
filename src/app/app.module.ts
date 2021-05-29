d999524a0a7e0f736883e9ef3e5d8a600b37cae7
Michael Navazhylau <mankbf@umsystem.edu>
2021-05-29 04:23:29 -0500

    fridge view card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import { NavHeaderComponent } from './share/nav-header/nav-header.component';
 import { ViewFridgeComponent } from './components/view-fridge/view-fridge.component';
 import { FridgeViewCardComponent } from './share/fridge-view-card/fridge-view-card.component';
-
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -22,7 +22,8 @@ import { FridgeViewCardComponent } from './share/fridge-view-card/fridge-view-ca
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/share/fridge-view-card/fridge-view-card.component.ts
+++ src/app/share/fridge-view-card/fridge-view-card.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FridgeItem } from 'src/app/models/FridgeItem';
 
 @Component({
-  selector: 'app-fridge-view-card',
+  selector: 'FridgeViewCard',
   templateUrl: './fridge-view-card.component.html',
   styleUrls: ['./fridge-view-card.component.scss']
 })
 export class FridgeViewCardComponent implements OnInit {
 
-  constructor() { }
+  @Input() public itemToView: FridgeItem = <FridgeItem>{
+    name: "",
+    quantity: 0,
+    units: "",
+    expirationDate: new Date(Date.UTC(2000,12,12))
+  };
+
+  constructor() { 
+
+  }
+
+  prettyDate(): string{
+    let month = this.itemToView.expirationDate.getMonth().toString();
+    let day = this.itemToView.expirationDate.getDay().toString();
+    let year = this.itemToView.expirationDate.getFullYear().toString();
+
+    return `${month}/${day}/${year}`;
+  }
 
   ngOnInit(): void {
+
   }
 
 }
